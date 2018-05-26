6f4fedd8de9d05b3dbc653c73af509198ff10b67
Ryan Zujic <rzujic@gmail.com>
2018-05-26 15:37:59 +1000

    Updated someStuff.py

--- someStuff.py
+++ someStuff.py
@@ -2,6 +2,8 @@ def printThings(things):
 	for thing in things:
 	print thing
 	
-listOfThings = ["Bob", "USB", "House" 3, 45]
+listOfThings = ["Bob", "USB", "House" 3, 45, 90]
 
 printThings(listOfThings)
+
+print "Completed"
